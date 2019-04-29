e2fce74b6c9d4481d34beec92a557149a71b6086
Almas Akchabayev <almeynman@gmail.com>
2019-04-29 18:39:46 +0300

    autoincrement and tag (#2)
    * autoincrement and tag
    
    * add simple git image
    
    * add new deploy key
    
    * resolve breaking change in ssh-keygen
    
    * fix lint error
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * fix updating version
    
    * log
    
    * add git fetch
    
    * add git pull
    
    * fix git pull
    
    * add git tag script incrementer
    
    * try using shell script in ci
    
    * try fix permission
    
    * get tags
    
    * try in one command
    
    * try do everything in a script
    
    * echo version
    
    * simplify script
    
    * try new describe command
    
    * try node
    
    * prepare for master

---
+++ scripts/increment-git-tag-patch.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#Update local tags
+git fetch --tags
+
+#Get the highest tag number
+VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
+VERSION=${VERSION:-'0.0.0'}
+echo $VERSION
+
+#Get number parts
+MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+
+#Increase version
+PATCH=$((PATCH+1))
+
+#Get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+
+#Create new tag
+NEW_TAG="$MAJOR.$MINOR.$PATCH"
+
+echo "Updating to $NEW_TAG"
+git tag $NEW_TAG
+echo "Pushing new tag $NEW_TAG"
+git push --tags
