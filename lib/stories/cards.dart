4b42a78876d111fdf4ed99e086c77356db2dd746
Vadim Gulyakin <vgulyakin@rightandabove.com>
2019-04-30 18:26:14 +0300

    added empty card sample

--- lib/stories/cards.dart
+++ lib/stories/cards.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_kit/color.dart';
+import 'package:flutter_ui_kit/story_book/expandable_story.dart';
 import 'package:flutter_ui_kit/story_book/prop_updater/double_prop_updater.dart';
 import 'package:flutter_ui_kit/story_book/props_explorer.dart';
-import 'package:flutter_ui_kit/story_book/story.dart';
 import 'package:flutter_ui_kit/widgets/card.dart';
 import 'package:flutter_ui_kit/widgets/text_button.dart';
 
@@ -13,16 +13,75 @@ class AppCards extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
+            _emptyCardStory(),
             _dialogCardStory(),
           ],
         ),
       ),
     );
   }
+  Widget _emptyCardStory() {
+    return ExpandableStory(
+      title: 'Empty Card',
+      child: PropsExplorer(
+        initialProps: const <String, dynamic>{
+          'elevation': 5.0,
+          'margin': 15.0,
+          'borderRadius': 4.0,
+        },
+        formBuilder: (context, props, updateProp) {
+          return ListView(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: <Widget>[
+              DoublePropUpdater(
+                  props: props,
+                  updateProp: updateProp,
+                  propKey: 'elevation',
+                  min: 0,
+                  max: 20
+              ),
+              DoublePropUpdater(
+                  props: props,
+                  updateProp: updateProp,
+                  propKey: 'margin',
+                  min: 0,
+                  max: 20
+              ),
+              DoublePropUpdater(
+                  props: props,
+                  updateProp: updateProp,
+                  propKey: 'borderRadius',
+                  min: 0,
+                  max: 20
+              )
+            ],
+          );
+        },
+        widgetBuilder: (context, props) {
+          final double elevation = props['elevation'];
+          final double margin = props['margin'];
+          final double borderRadius = props['borderRadius'];
+
+          return AppCard(
+            color: AppColor.deepWhite,
+            elevation: elevation,
+            margin: EdgeInsets.all(margin),
+            borderRadius: borderRadius,
+            child: Container(
+              width: 140,
+              height: 140,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
 
   Widget _dialogCardStory() {
-    return Story(
-      title: 'Card',
+    return ExpandableStory(
+      title: 'Sample Card',
       child: PropsExplorer(
       initialProps: const <String, dynamic>{
         'elevation': 5.0,
