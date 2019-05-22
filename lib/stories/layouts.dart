00f611c8f67f0a94887c1db0452dfad3d26b2b42
Vinay Puranik <vinaypuranik564@gmail.com>
2019-05-22 16:45:26 +0300

    added info-template

--- lib/stories/layouts.dart
+++ lib/stories/layouts.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_ui_kit/widgets/filled_button.dart';
 import 'package:flutter_ui_kit/widgets/layout/generic_template.dart';
+import 'package:flutter_ui_kit/widgets/layout/info_template.dart';
 import 'package:flutter_ui_kit/widgets/layout/utility_template.dart';
 import 'package:flutter_ui_kit/widgets/text/heading.dart';
 import 'package:flutter_ui_kit/widgets/text/heading_type.dart';
+import 'package:flutter_ui_kit/widgets/text/label_value_pair.dart';
 
 class Layouts extends StatelessWidget {
   static const String utilityLayoutRoute = '/utility-layout-page';
   static const String dismissibleLayoutRoute = '/dismissible-layout-page';
+  static const String infoLayoutRoute = '/info-layout-page';
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +23,7 @@ class Layouts extends StatelessWidget {
           children: [
             _utilityLayout(context),
             _dismissibleLayout(context),
+            _infoLayout(context)
           ],
         ),
       ),
@@ -73,7 +77,9 @@ class Layouts extends StatelessWidget {
                 MaterialPageRoute<dynamic>(
                     settings: const RouteSettings(name: dismissibleLayoutRoute),
                     builder: (_) => GenericTemplate(
-                        const Heading('How can we make Change better?', headingType: HeadingType.HEADING_1, textAlign: TextAlign.left),
+                        const Heading('How can we make Change better?',
+                            headingType: HeadingType.HEADING_1,
+                            textAlign: TextAlign.left),
                         Padding(
                             padding: const EdgeInsets.only(top: 20.0),
                             child: TextFormField(
@@ -89,7 +95,9 @@ class Layouts extends StatelessWidget {
                               autofocus: true,
                               onSaved: (String value) {},
                             )),
-                        FilledButton('SEND', fullWidth: true, onPressed: (){ print('you clicked send'); }),
+                        FilledButton('SEND', fullWidth: true, onPressed: () {
+                          print('you clicked send');
+                        }),
                         dismissible: true)));
           },
           fullWidth: false,
@@ -97,5 +105,40 @@ class Layouts extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _infoLayout(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Center(
+            child:
+                FilledButton('I\'m all about that info button', onPressed: () {
+          Navigator.push<dynamic>(
+              context,
+              MaterialPageRoute<dynamic>(
+                  settings: const RouteSettings(name: infoLayoutRoute),
+                  builder: (_) => InfoTemplate(
+                        const Heading(
+                            'Invite a friend and you both get €10 worth of Bitcoin',
+                            headingType: HeadingType.HEADING_2,
+                            textAlign: TextAlign.left),
+                        const Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: LabelValuePair(
+                              labelText: 'Invite Link',
+                              valueText: 'getchange.com/r/2415',
+                              copyToClipboardEnabled: true
+                          ),
+                        ),
+                        FilledButton('Invite friends', fullWidth: true,
+                            onPressed: () {
+                          print('you clicked invite friends');
+                        }),
+                        info: true,
+                        routeName: utilityLayoutRoute,
+                        picture: SvgPicture.network(
+                            'https://s3.eu-central-1.amazonaws.com/production-change-app-assets/images/referral-screen.svg'),
+                      )));
+        })));
   }
 }
