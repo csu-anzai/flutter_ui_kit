cfbe7d9935db440e493ea8c8d326912c1cfbf27b
SZdobnikova <38031386+SZdobnikova@users.noreply.github.com>
2019-07-25 16:28:48 +0300

    Dope 166 date of birth wizard page fixes (#58)
    * changes and fixes
    
    * fixed issue with keyboard;
    add next button
    
    * add callback to get selected value
    
    * fix for failed build
    
    * add key values to text fields
    
    * init dat time set null
    
    * one more fix
    
    * requested changes
    
    * changes for custom date picker

--- lib/widgets/custom_date_picker.dart
+++ lib/widgets/custom_date_picker.dart
@@ -1,99 +1,113 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
 
-class CustomDatePicker extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _DatePickerBottomSheetState();
-}
+typedef Callback = void Function(DateTime value);
 
-class _DatePickerBottomSheetState extends State<CustomDatePicker> {
+class CustomDatePicker extends StatelessWidget {
   final _format = 'dd-MM-yyyy';
   final _dayCtrl = TextEditingController();
   final _monthCtrl = TextEditingController();
   final _yearCtrl = TextEditingController();
+  final Callback onChange;
 
-  DateTime _dateTime;
+  CustomDatePicker({@required this.onChange});
+
+  static DateTime _dateTime;
 
   @override
-  void initState() {
-    super.initState();
-    _dayCtrl.text = null;
-    _monthCtrl.text = null;
-    _yearCtrl.text = null;
-    _dateTime = DateTime.now();
+  Widget build(BuildContext context) {
+    return
+//      Column(children: <Widget>[
+        Container(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+          new Flexible(
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Container(
+                      width: 60,
+                      child: TextField(
+                          focusNode: DisabledFocusNode(),
+                          controller: _dayCtrl,
+                          key: const Key('day-of-birth'),
+                          keyboardType: TextInputType.datetime,
+                          decoration: const InputDecoration(
+                            labelText: 'Day',
+                            hintText: 'DD',
+                            hintStyle: const TextStyle(color: Colors.white30),
+                          ),
+                          onTap: () {
+                            _showDatePicker(context);
+                          })))),
+          new Flexible(
+              child: Container(
+                  width: 80,
+                  child: TextField(
+                    focusNode: DisabledFocusNode(),
+                    controller: _monthCtrl,
+                    key: const Key('month-of-birth'),
+                    keyboardType: TextInputType.datetime,
+                    decoration: const InputDecoration(
+                      labelText: 'Month',
+                      hintText: 'MM',
+                      hintStyle: const TextStyle(color: Colors.black26),
+                    ),
+                    onTap: () {
+                      _showDatePicker(context);
+                    },
+                  ))),
+          new Flexible(
+              child: Padding(
+                  padding: const EdgeInsets.only(right: 30.00),
+                  child: TextField(
+                    focusNode: DisabledFocusNode(),
+                    controller: _yearCtrl,
+                    key: const Key('year-of-birth'),
+                    keyboardType: TextInputType.datetime,
+                    decoration: const InputDecoration(
+                      labelText: 'Year',
+                      hintText: 'YYYY',
+                      hintStyle: const TextStyle(color: Colors.black26),
+                    ),
+                    onTap: () {
+                      _showDatePicker(context);
+                    },
+                  )))
+        ]));
   }
 
-  /// Display time picker.
-  void _showDatePicker() {
+  void _showDatePicker(BuildContext context) {
     DatePicker.showDatePicker(
       context,
-      initialDateTime: DateTime.now(),
+      initialDateTime: _dateTime != null ? DateTime.now() : _dateTime,
       dateFormat: _format,
       onCancel: () {
-        updateState(DateTime.now());
+        updateState(_dateTime);
       },
       onChange: (dateTime, List<int> index) {
         updateState(dateTime);
       },
       onConfirm: (dateTime, List<int> index) {
         updateState(dateTime);
+        _dateTime = dateTime;
+        onChange(dateTime);
       },
     );
   }
 
   void updateState(DateTime dateTime) {
-    setState(() {
-      _dateTime = dateTime;
-      _dayCtrl.text = _dateTime.day.toString().padLeft(2, '0');
-      _monthCtrl.text = _dateTime.month.toString().padLeft(2, '0');
-      _yearCtrl.text = _dateTime.year.toString();
-    });
+    _dayCtrl.text =
+        dateTime != null ? dateTime.day.toString().padLeft(2, '0') : dateTime;
+    _monthCtrl.text =
+        dateTime != null ? dateTime.month.toString().padLeft(2, '0') : dateTime;
+    _yearCtrl.text = dateTime != null ? dateTime.year.toString() : dateTime;
   }
+}
 
+class DisabledFocusNode extends FocusNode {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        new Flexible(
-            child: Padding(
-                padding: const EdgeInsets.only(left: 30.0),
-                child: Container(
-                    width: 60,
-                    child: TextField(
-                      controller: _dayCtrl,
-                      decoration: const InputDecoration(
-                        labelText: 'Day',
-                        hintText: 'DD',
-                        hintStyle: const TextStyle(color: Colors.white30),
-                      ),
-                      onTap: _showDatePicker,
-                    )))),
-        new Flexible(
-            child: Container(
-                width: 60,
-                child: TextField(
-                  controller: _monthCtrl,
-                  decoration: const InputDecoration(
-                    labelText: 'Month',
-                    hintText: 'MM',
-                    hintStyle: const TextStyle(color: Colors.black26),
-                  ),
-                  onTap: _showDatePicker,
-                ))),
-        new Flexible(
-            child: Padding(
-                padding: const EdgeInsets.only(right: 30.00),
-                child: TextField(
-                  controller: _yearCtrl,
-                  decoration: const InputDecoration(
-                    labelText: 'Year',
-                    hintText: 'YYYY',
-                    hintStyle: const TextStyle(color: Colors.black26),
-                  ),
-                  onTap: _showDatePicker,
-                )))
-      ]),
-    );
+  bool consumeKeyboardToken() {
+    return false;
   }
 }
