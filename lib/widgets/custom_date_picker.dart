b9ffedbc74a25b917050d89c8db8b4a4bf7ea6bc
SZdobnikova <szdobnikova@rightandabove.com>
2019-07-25 14:30:14 +0300

    changes for custom date picker

--- lib/widgets/custom_date_picker.dart
+++ lib/widgets/custom_date_picker.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
-
-import 'filled_button.dart';
 
 typedef Callback = void Function(DateTime value);
 
 class CustomDatePicker extends StatelessWidget {
-
   final _format = 'dd-MM-yyyy';
   final _dayCtrl = TextEditingController();
   final _monthCtrl = TextEditingController();
   final _yearCtrl = TextEditingController();
   final Callback onChange;
 
-  CustomDatePicker({ @required this.onChange});
+  CustomDatePicker({@required this.onChange});
 
   static DateTime _dateTime;
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(
-          child:
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            new Flexible(
-                child: Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: Container(
-                        width: 60,
-                        child: TextField(
+    return
+//      Column(children: <Widget>[
+        Container(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+          new Flexible(
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Container(
+                      width: 60,
+                      child: TextField(
                           focusNode: DisabledFocusNode(),
                           controller: _dayCtrl,
                           key: const Key('day-of-birth'),
@@ -39,81 +37,71 @@ class CustomDatePicker extends StatelessWidget {
                             hintText: 'DD',
                             hintStyle: const TextStyle(color: Colors.white30),
                           ),
-                            onTap: () {
-                              _showDatePicker(context);
-                            }
-                        )))),
-            new Flexible(
-                child: Container(
-                    width: 80,
-                    child: TextField(
-                      focusNode: DisabledFocusNode(),
-                      controller: _monthCtrl,
-                      key: const Key('month-of-birth'),
-                      keyboardType: TextInputType.datetime,
-                      decoration: const InputDecoration(
-                        labelText: 'Month',
-                        hintText: 'MM',
-                        hintStyle: const TextStyle(color: Colors.black26),
-                      ),
-                      onTap: () {
-                        _showDatePicker(context);
-                      },
-                    ))),
-            new Flexible(
-                child: Padding(
-                    padding: const EdgeInsets.only(right: 30.00),
-                    child: TextField(
-                      focusNode: DisabledFocusNode(),
-                      controller: _yearCtrl,
-                      key: const Key('year-of-birth'),
-                      keyboardType: TextInputType.datetime,
-                      decoration: const InputDecoration(
-                        labelText: 'Year',
-                        hintText: 'YYYY',
-                        hintStyle: const TextStyle(color: Colors.black26),
-                      ),
-                      onTap: () {
-                        _showDatePicker(context);
-                      },
-                    )
-                )
-            )
-          ]
-          )
-      ),
-      Padding(
-          padding: const EdgeInsets.only(top: 160.00),
-          child: FilledButton(
-            'Next',
-            onPressed: () {
-              print('${_dayCtrl.text}-${_monthCtrl.text}-${_yearCtrl.text}');
-              onChange(_dateTime);
-            },
-          ))
-    ]);
+                          onTap: () {
+                            _showDatePicker(context);
+                          })))),
+          new Flexible(
+              child: Container(
+                  width: 80,
+                  child: TextField(
+                    focusNode: DisabledFocusNode(),
+                    controller: _monthCtrl,
+                    key: const Key('month-of-birth'),
+                    keyboardType: TextInputType.datetime,
+                    decoration: const InputDecoration(
+                      labelText: 'Month',
+                      hintText: 'MM',
+                      hintStyle: const TextStyle(color: Colors.black26),
+                    ),
+                    onTap: () {
+                      _showDatePicker(context);
+                    },
+                  ))),
+          new Flexible(
+              child: Padding(
+                  padding: const EdgeInsets.only(right: 30.00),
+                  child: TextField(
+                    focusNode: DisabledFocusNode(),
+                    controller: _yearCtrl,
+                    key: const Key('year-of-birth'),
+                    keyboardType: TextInputType.datetime,
+                    decoration: const InputDecoration(
+                      labelText: 'Year',
+                      hintText: 'YYYY',
+                      hintStyle: const TextStyle(color: Colors.black26),
+                    ),
+                    onTap: () {
+                      _showDatePicker(context);
+                    },
+                  )))
+        ]));
   }
+
   void _showDatePicker(BuildContext context) {
     DatePicker.showDatePicker(
       context,
-      initialDateTime: DateTime.now(),
+      initialDateTime: _dateTime != null ? DateTime.now() : _dateTime,
       dateFormat: _format,
       onCancel: () {
-        updateState(DateTime.now());
+        updateState(_dateTime);
       },
       onChange: (dateTime, List<int> index) {
         updateState(dateTime);
       },
       onConfirm: (dateTime, List<int> index) {
         updateState(dateTime);
+        _dateTime = dateTime;
+        onChange(dateTime);
       },
     );
   }
+
   void updateState(DateTime dateTime) {
-    _dateTime = dateTime;
-    _dayCtrl.text = _dateTime.day.toString().padLeft(2, '0');
-    _monthCtrl.text = _dateTime.month.toString().padLeft(2, '0');
-    _yearCtrl.text = _dateTime.year.toString();
+    _dayCtrl.text =
+        dateTime != null ? dateTime.day.toString().padLeft(2, '0') : dateTime;
+    _monthCtrl.text =
+        dateTime != null ? dateTime.month.toString().padLeft(2, '0') : dateTime;
+    _yearCtrl.text = dateTime != null ? dateTime.year.toString() : dateTime;
   }
 }
 
