314173916453ee19ae06e35ec57f72435005d719
sanderheinsar <sHeinsar@gmail.com>
2019-07-29 12:18:19 +0300

    Small name fixes

--- lib/widgets/wizzard/wizard.dart
+++ lib/widgets/wizzard/wizard.dart
@@ -1,36 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_kit/widgets.dart';
 import 'package:flutter_ui_kit/widgets/layout/app_bar.dart';
 import 'package:flutter_ui_kit/widgets/layout/page_template.dart';
+import 'package:flutter_ui_kit/widgets/simple_progress_indicator.dart';
 import 'package:flutter_ui_kit/widgets/wizzard/wizard_binder.dart';
 
 import '../filled_button.dart';
 
 class Wizard extends StatefulWidget {
-  
   final WizardBinder binder;
   final Widget intoWidget;
   final List<Widget> wizardPages;
   final Widget outroWidget;
 
-  const Wizard({this.intoWidget, this.wizardPages, this.outroWidget, this.binder});
+  const Wizard(
+      {this.intoWidget, this.wizardPages, this.outroWidget, this.binder});
 
   @override
-  _WizardState createState() => _WizardState(intoWidget, wizardPages, outroWidget, binder);
+  _WizardState createState() =>
+      _WizardState(intoWidget, wizardPages, outroWidget, binder);
 }
 
 class _WizardState extends State<Wizard> {
-  
   WizardBinder binder;
   int currentPage = -1;
 
-  Widget intoWidget;
+  Widget introWidget;
   List<Widget> wizardPages;
   Widget outroWidget;
+  final List<int> skippedPages = [];
 
   Widget leadingWidget = const BackButton();
   List<Widget> tailingWidget;
 
-  _WizardState(this.intoWidget, this.wizardPages, this.outroWidget, this.binder);
+  _WizardState(
+      this.introWidget, this.wizardPages, this.outroWidget, this.binder);
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +42,7 @@ class _WizardState extends State<Wizard> {
       appBar: MainAppBar(
           implyLeading: leadingWidget != null,
           leadingWidget: leadingWidget,
-          tailingWidget: tailingWidget),
+          tailingWidget: [_doThisLater()]),
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
@@ -49,17 +53,43 @@ class _WizardState extends State<Wizard> {
                 _callToActionEvent();
                 currentPage++;
               });
-            })
+            }),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, left: 10.0, right: 10),
+              child: Container(
+                  alignment: Alignment.bottomCenter,
+                  child: _saveProgressIndicatorState()),
+            ),
           ],
         ),
       ),
     );
   }
 
+  Widget _doThisLater() {
+    if (currentPage >= 0 && currentPage <= wizardPages.length) {
+      return TextButton('Do this later', onPressed: () {
+      });
+    }
+    return TextButton('');
+  }
+
+  Widget _saveProgressIndicatorState() {
+    if (currentPage >= 0 && currentPage <= wizardPages.length) {
+      return SimpleProgressIndicator(
+        height: 3,
+        percentageFilled: currentPage / wizardPages.length * 100,
+      );
+    }
+    return const SimpleProgressIndicator(
+      percentageFilled: 0,
+    );
+  }
+
   void _callToActionEvent() {
     if (currentPage < 0) {
       return binder.onStart();
-    } else if(currentPage >= wizardPages.length) {
+    } else if (currentPage >= wizardPages.length) {
       return binder.onEnd();
     } else {
       binder.onNext(currentPage);
@@ -67,11 +97,11 @@ class _WizardState extends State<Wizard> {
   }
 
   String _callToActionText() {
-    final text =  binder.getCallToAction(currentPage);
+    final text = binder.getCallToAction(currentPage);
     if (text == null) {
       if (currentPage < 0) {
-        return 'Start Started';
-      } else if(currentPage >= wizardPages.length) {
+        return 'Create account';
+      } else if (currentPage == wizardPages.length) {
         return 'End';
       } else {
         return 'Next';
@@ -84,8 +114,8 @@ class _WizardState extends State<Wizard> {
   Widget _getCurrentPage() {
     if (currentPage < 0) {
       debugPrint('Showing intro wizard');
-      return intoWidget;
-    } else if(currentPage >= wizardPages.length) {
+      return introWidget;
+    } else if (currentPage >= wizardPages.length) {
       debugPrint('Showing outro wizard');
       return outroWidget;
     } else {
